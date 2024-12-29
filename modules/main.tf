600265c3eb9df445b3268787f2b36c0ab3500f2b
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2024-12-29 12:07:42 +0530

    Push

--- modules/main.tf
+++ modules/main.tf
@@ -11,7 +11,7 @@ resource "google_container_cluster" "primary" {
 
   # We can't create a cluster with no node pool defined, but we want to only use
   # separately managed node pools. So we create the smallest possible default
-  # node pool and immediately delete it.
+  # node pool and immediately delete it. ok
   remove_default_node_pool = true
   initial_node_count       = 1
   workload_identity_config {
